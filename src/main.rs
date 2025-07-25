944fa70b920ba1b768207d3533b4b5211f561f46
Nando <tarin.f@gmail.com>
2025-07-25 22:31:36 +0900

    Added the option to save and restore the model weights when training

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![recursion_limit = "256"]
 
 use std::fs;
+use std::path::PathBuf;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use argh::FromArgs;
+use burn::module::Module;
 use burn::prelude::Backend;
+use burn::record::{FullPrecisionSettings, NamedMpkGzFileRecorder};
 use rust_tokenizers::tokenizer::Gpt2Tokenizer;
 
 use crate::gpt2_model::{GPTModelConfig, GPTModel};
@@ -27,6 +30,10 @@ mod utils;
 #[derive(FromArgs)]
 /// Options to train the model.
 struct GptOptions {
+    /// select the mode to run the tool (generate, train)
+    #[argh(positional)]
+    command: String,
+
     /// context length for the GPT Model (1024 by default).
     #[argh(option, default = "1024")]
     context_length: usize,
@@ -74,11 +81,14 @@ struct GptOptions {
     /// train ratio for training (0.9 by default).
     #[argh(option, default = "0.9")]
     train_ratio: f32,
+
+    /// file path for the model weights (weights by default)
+    #[argh(option)]
+    weights: String,
+
 }
 
-fn main() {
-    let args: GptOptions = argh::from_env();
-
+fn train_command(args: GptOptions) {
     println!("--- Tokenizer Summary ---");
     println!("\tUsing vocabulary file: {}", args.vocab_file);
     println!("\tUsing merges file: {}", args.merges_file);
@@ -94,7 +104,7 @@ fn main() {
     println!("\tUsing file {} ", args.training_file_name);
     let raw_text: String = fs::read_to_string(args.training_file_name).expect("Training file must exist");
     let train_size = (raw_text.len() as f32 * args.train_ratio) as usize;
-    println!("\tTraining size: {}", train_size);
+    println!("\tTraining size: {train_size}");
     println!("\tValidation size: {}", raw_text.len() - train_size);
 
     println!("--- Training Summary ---");
@@ -122,6 +132,18 @@ fn main() {
         0.1,
         false).init(device);
 
+    if !args.weights.is_empty() {
+        let path = PathBuf::from(args.weights.clone() + ".mpk.gz");
+        let recorder = NamedMpkGzFileRecorder::<FullPrecisionSettings>::new();
+        if path.exists(){
+            let basename_path = PathBuf::from(&args.weights);
+            println!("\tLoading weights from {:?}", path);
+            gpt2_model = gpt2_model
+                            .load_file(basename_path, &recorder, device)
+                            .expect("Should be able to load the model weights from the provided file")
+        }
+    }
+
     println!("--- Training Model ---");
     let training_options = TrainingOptions {
         max_seq_len: args.context_length,
@@ -138,4 +160,31 @@ fn main() {
 
     println!("--- Generating Text After Training ---");
     println!("\t{}", generate_text(&gpt2_model, &tokenizer, &args.text_to_continue, 25, args.context_length, 0.8, 20));
+
+    if !args.weights.is_empty() {
+        println!("--- Saving Model Weights ---");
+        println!("\tSaving weights to {:?}", path);
+        let path = PathBuf::from(args.weights.clone() + ".mpk.gz");
+        let basename_path = PathBuf::from(&args.weights);
+        let recorder = NamedMpkGzFileRecorder::<FullPrecisionSettings>::new();
+        gpt2_model
+            .save_file(basename_path, &recorder)
+            .expect("Should be able to save the model");
+        }
+}
+
+
+fn generate_command(args: GptOptions) {
+    println!("ToDo");
+}
+
+fn main() {
+    let args: GptOptions = argh::from_env();
+
+    if args.command == "train" {
+        train_command(args);
+    } else{
+        generate_command(args);
+    }
+
 }
